1530bc52e6aa2d681de1b02285f6412a588803c6
CamilleAbella <camille.abella@hotmail.fr>
2020-07-20 12:13:30 +0200

    add some series, add flags and synopsis, move nav on left and right sides.

--- functions/newPopup.js
+++ functions/newPopup.js
@@ -3,33 +3,30 @@ module.exports = function newPopup(anime) {
     `
       <h2> ${anime.name} </h2>
       <div class="frame">
+        <div
+          class="prev"
+          onclick="openPopup(document.getElementById('anime-${anime.prev}'))">
+          <div class="icon-button">&blacktriangleleft;</div>
+        </div>
         <img
           src="${anime.image}"
           alt="${anime.name.toLowerCase()} image">
+         <div
+          class="next"
+          onclick="openPopup(document.getElementById('anime-${anime.next}'))">
+          <div class="icon-button">&blacktriangleright;</div>
+        </div>
       </div>
       <div class="wrapper flags">
         ${anime.flags.map((c) => "<div>" + c + "</div>").join("")}
       </div>
     ` +
-    (anime.comment.length > 0
+    (anime.comments.length > 0
       ? `
         <div class="comments">
-          ${anime.comment.map((c) => "<div>" + c + "</div>").join("")}
+          ${anime.comments.map((c) => "<div>" + c + "</div>").join("")}
         </div>
       `
-      : "") +
-    `
-    <div class='nav'>
-      <div
-        class="prev"
-        onclick="openPopup(document.getElementById('anime-${anime.prev}'))">
-        <div>Précédent</div>
-      </div>
-      <div
-        class="next"
-        onclick="openPopup(document.getElementById('anime-${anime.next}'))">
-        <div>Suivant</div>
-      </div>
-    </div>`
+      : "")
   );
 };
